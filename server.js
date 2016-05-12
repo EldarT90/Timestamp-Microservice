68e8b08c79ca04b007996b9c15288df148a7914a
Eldar Tinjić <tica_bh@hotmail.com>
2016-05-12 16:00:41 +0000

    Port changes

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
     res.send("test");
 });
 
-app.listen(8080, function () {
-    console.log('Listening on port 8080...');
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Listening on port ' + process.env.PORT + '...');
 });
